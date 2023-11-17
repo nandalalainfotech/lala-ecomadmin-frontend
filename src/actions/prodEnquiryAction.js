69e4606e070082ec37c0b619815ca0d9e7f456e5
arunkumar030 <arunrkumar08@gmail.com>
2023-11-17 19:22:32 +0530

    Notification alert code implemented done..

--- src/actions/prodEnquiryAction.js
+++ src/actions/prodEnquiryAction.js
@@ -1,14 +1,107 @@
 import Axios from "axios";
-import { PPRODUCT_ENQUIRY_LIST_FAIL, PPRODUCT_ENQUIRY_LIST_REQUEST, PPRODUCT_ENQUIRY_LIST_SUCCESS } from "../constants/prodEnquiryConstant";
+import {
+  PPRODUCT_ENQUIRY_LIST_FAIL, PPRODUCT_ENQUIRY_LIST_REQUEST, PPRODUCT_ENQUIRY_LIST_SUCCESS, NOTIFICATION_DELETE_FAIL,
+  NOTIFICATION_DELETE_REQUEST,
+  NOTIFICATION_DELETE_SUCCESS,
+  NOTIFICATION_CLEAR_FAIL,
+  NOTIFICATION_CLEAR_REQUEST,
+  NOTIFICATION_CLEAR_SUCCESS,
+  PRODUCT_NOTIFICATION_LIST_REQUEST,
+  PRODUCT_NOTIFICATION_LIST_SUCCESS,
+  PRODUCT_NOTIFICATION_LIST_FAIL,
+  PRODUCT_ENQUIRY_DELETE_FAIL,
+  PRODUCT_ENQUIRY_DELETE_REQUEST,
+  PRODUCT_ENQUIRY_DELETE_SUCCESS,
+} from "../constants/prodEnquiryConstant";
 
 export const prodEnquiryList = () => async (dispatch) => {
-    dispatch({
-      type: PPRODUCT_ENQUIRY_LIST_REQUEST,
+  dispatch({
+    type: PPRODUCT_ENQUIRY_LIST_REQUEST,
+  });
+  try {
+    const { data } = await Axios.get(`/api/prodenquiry/enquiryList`);
+    dispatch({ type: PPRODUCT_ENQUIRY_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: PPRODUCT_ENQUIRY_LIST_FAIL, payload: error.message });
+  }
+};
+
+export const deletenotification = (delId) => async (dispatch) => {
+  dispatch({ type: NOTIFICATION_DELETE_REQUEST, payload: delId });
+
+  try {
+    await Axios.delete(`/api/prodenquiry/notification/${delId}`);
+
+    dispatch({ type: NOTIFICATION_DELETE_SUCCESS });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: NOTIFICATION_DELETE_FAIL, payload: message });
+  }
+};
+
+export const clearnotification = (clearid) => async (dispatch) => {
+  dispatch({ type: NOTIFICATION_CLEAR_REQUEST, payload: clearid });
+  try {
+    await Axios.delete(`/api/prodenquiry/clear/`, {
+      data: clearid,
     });
-    try {
-      const { data } = await Axios.get(`/api/prodenquiry/enquiryList`);
-      dispatch({ type: PPRODUCT_ENQUIRY_LIST_SUCCESS, payload: data });
-    } catch (error) {
-      dispatch({ type: PPRODUCT_ENQUIRY_LIST_FAIL, payload: error.message });
-    }
-  };
+
+    dispatch({ type: NOTIFICATION_CLEAR_SUCCESS });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: NOTIFICATION_CLEAR_FAIL, payload: message });
+  }
+};
+
+export const notificationtest = () => async (dispatch) => {
+  dispatch({
+    type: PRODUCT_NOTIFICATION_LIST_REQUEST,
+  });
+  try {
+    const { data } = await Axios.get(`/api/prodenquiry/notification`);
+    dispatch({ type: PRODUCT_NOTIFICATION_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: PRODUCT_NOTIFICATION_LIST_FAIL, payload: error.message });
+  }
+};
+
+export const enquiryresponce = (response) => async (dispatch) => {
+  dispatch({ type: PPRODUCT_ENQUIRY_LIST_REQUEST, payload: response });
+  try {
+    const { data } = await Axios.post(
+      `/api/prodenquiry/enquiryresponce`,
+      response,
+    );
+    dispatch({ type: PPRODUCT_ENQUIRY_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: PPRODUCT_ENQUIRY_LIST_FAIL, payload: message });
+  }
+};
+
+export const deleteenquiry = (delId) => async (dispatch) => {
+  dispatch({ type: PRODUCT_ENQUIRY_DELETE_REQUEST, payload: delId });
+  try {
+    const { data } = await Axios.delete(
+      `/api/prodenquiry/enquiryList/${delId}`,
+    );
+    dispatch({ type: PRODUCT_ENQUIRY_DELETE_SUCCESS, payload: data });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: PRODUCT_ENQUIRY_DELETE_FAIL, payload: message });
+  }
+};
+
+
--- src/reducers/prodEnquiryReducer.js
+++ src/reducers/prodEnquiryReducer.js
@@ -1,12 +1,27 @@
 import {
+  NOTIFICATION_CLEAR_FAIL,
+  NOTIFICATION_CLEAR_REQUEST,
+  NOTIFICATION_CLEAR_RESET,
+  NOTIFICATION_CLEAR_SUCCESS,
+  NOTIFICATION_DELETE_FAIL,
+  NOTIFICATION_DELETE_REQUEST,
+  NOTIFICATION_DELETE_RESET,
+  NOTIFICATION_DELETE_SUCCESS,
   PPRODUCT_ENQUIRY_LIST_FAIL,
   PPRODUCT_ENQUIRY_LIST_REQUEST,
   PPRODUCT_ENQUIRY_LIST_SUCCESS,
+  PRODUCT_ENQUIRY_DELETE_FAIL,
+  PRODUCT_ENQUIRY_DELETE_REQUEST,
+  PRODUCT_ENQUIRY_DELETE_RESET,
+  PRODUCT_ENQUIRY_DELETE_SUCCESS,
+  PRODUCT_NOTIFICATION_LIST_FAIL,
+  PRODUCT_NOTIFICATION_LIST_REQUEST,
+  PRODUCT_NOTIFICATION_LIST_SUCCESS,
 } from "../constants/prodEnquiryConstant";
 
 export const productEnquiryListReducer = (
   state = { loading: true, prodEnqList: [] },
-  action
+  action,
 ) => {
   switch (action.type) {
     case PPRODUCT_ENQUIRY_LIST_REQUEST:
@@ -22,3 +37,68 @@ export const productEnquiryListReducer = (
       return state;
   }
 };
+
+export const enquiryDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_ENQUIRY_DELETE_REQUEST:
+      return { loading: true };
+    case PRODUCT_ENQUIRY_DELETE_SUCCESS:
+      return { loading: false, success: true };
+    case PRODUCT_ENQUIRY_DELETE_FAIL:
+      return { loading: false, error: action.payload };
+    case PRODUCT_ENQUIRY_DELETE_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const productNotificationListReducer = (
+  state = { loading: true, notificationlist: [] },
+  action,
+) => {
+  switch (action.type) {
+    case PRODUCT_NOTIFICATION_LIST_REQUEST:
+      return { loading: true };
+    case PRODUCT_NOTIFICATION_LIST_SUCCESS:
+      return {
+        loading: false,
+        notificationlist: action.payload,
+      };
+    case PRODUCT_NOTIFICATION_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const notificationDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case NOTIFICATION_DELETE_REQUEST:
+      return { loading: true };
+    case NOTIFICATION_DELETE_SUCCESS:
+      return { loading: false, success: true };
+    case NOTIFICATION_DELETE_FAIL:
+      return { loading: false, error: action.payload };
+    case NOTIFICATION_DELETE_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const notificationClearallReducer = (state = {}, action) => {
+  switch (action.type) {
+    case NOTIFICATION_CLEAR_REQUEST:
+      return { loading: true };
+    case NOTIFICATION_CLEAR_SUCCESS:
+      return { loading: false, success: true };
+    case NOTIFICATION_CLEAR_FAIL:
+      return { loading: false, error: action.payload };
+    case NOTIFICATION_CLEAR_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
